5e95d83034077ad2dd994a51b3c9bfb45884e161
Vyacheslav Yarkin <yarkin.vyacheslav@mail.ru>
2021-09-10 19:46:55 +0300

    entrypoiny typo fix

--- seminar2/entrypoint.sh
+++ seminar2/entrypoint.sh
@@ -7,3 +7,4 @@ source $FREESURFER_HOME/SetUpFreeSurfer.sh
 printf 'FREESURFER - check\n' 
 
 ./opt/miniconda-latest/envs/neuro/bin/jupyter-lab --ip=0.0.0.0 --port=8080 --no-browser --allow-root --notebook-dir='/workspace' --NotebookApp.token=''
+./opt/miniconda-latest/envs/neuro/bin/jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser --allow-root --notebook-dir='/workspace' --NotebookApp.token=''
